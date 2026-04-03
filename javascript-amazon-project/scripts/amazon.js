928f3bb00a42fbfb919907880ce86509a51c6b19
Anuj Dixit <anuj29.2dixit@gmail.com>
2026-04-04 04:23:37 +0530

    adding data files in amazone project

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -49,7 +49,7 @@ products.forEach((product) =>{
                 Added
               </div>
 
-              <button class="add-to-cart-button button-primary">
+              <button class="add-to-cart-button button-primary js-add-to-cart">
                 Add to Cart
               </button>
             </div>
@@ -60,3 +60,10 @@ products.forEach((product) =>{
 console.log(productsHTML);
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log('Added product');
+  });
+});
